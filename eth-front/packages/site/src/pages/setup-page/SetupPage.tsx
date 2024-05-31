ca24ce77107d224f8e3563c42a891e09723cc343
lukalazarevic123 <lazarevicluka99@gmail.com>
2024-05-31 19:09:39 +0100

    Generate stealth key 🥷

--- eth-front/packages/site/src/pages/setup-page/SetupPage.tsx
+++ eth-front/packages/site/src/pages/setup-page/SetupPage.tsx
@@ -10,6 +10,8 @@ import {
 import { isLocalSnap, shouldDisplayReconnectButton } from '../../utils';
 import styled from 'styled-components';
 import { ethers, hexlify, toUtf8Bytes } from 'ethers';
+import { Maybe } from '@metamask/providers/dist/types/utils';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Container = styled.div`
   display: flex;
@@ -74,7 +76,9 @@ const SignTitle = styled.div`
 `;
 
 const SetupPage = () => {
-  const [message, setMessage] = useState("Sign this message to access your Myst account.");
+  const [message, setMessage] = useState(
+    'Sign this message to access your Myst account.',
+  );
 
   const { error, provider } = useMetaMaskContext();
   const { isFlask, snapsDetected, installedSnap } = useMetaMask();
@@ -105,32 +109,49 @@ const SetupPage = () => {
   const signMessage = async () => {
     const msg = hexlify(toUtf8Bytes(message));
 
-    // const resp = await invokeSnap({method: 'personal_sign', params: {
-    //     msg, from: "0x9EdC84691580CBD5ED56Ff0e90390A0857FB3D49"
-    // }})
-
-    const accounts = await provider?.request({
-        method: 'eth_requestAccounts'
-    })
-
-    if(!accounts) return;
-
-    console.log(accounts)
-
-    const rpcSnap = await provider?.request({
-        method: 'personal_sign',
-        // @ts-ignore
-        params: [msg, accounts[0]]
+    const accounts: Maybe<string[]> = await provider?.request({
+      method: 'eth_requestAccounts',
     });
 
-    console.log(rpcSnap)
-  }
+    if (!accounts || !accounts[0])
+      return toast.error("Can't acquire users accounts");
+
+    const signature: Maybe<string> = await provider?.request({
+      method: 'personal_sign',
+      params: [msg, accounts[0]],
+    });
+
+    if (!signature) return toast.error('Error signing the message');
+
+    const sig1 = signature.slice(2, 66);
+    const sig2 = signature.slice(66, 130);
+
+    const hashedV = ethers.sha256('0x' + sig1);
+    const hashedR = ethers.sha256('0x' + sig2);
+    const n = ethers.getBigInt(
+      '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141',
+    );
+
+    const privateKey1 = ethers.getBigInt(hashedV) % n;
+    const privateKey2 = ethers.getBigInt(hashedR) % n;
+
+    const keyPair1 = new ethers.Wallet("0x" + privateKey1.toString(16));
+    const keyPair2 = new ethers.Wallet("0x" + privateKey2.toString(16));
+
+    const spendingPublicKey = keyPair1.signingKey.compressedPublicKey
+    const viewingPublicKey = keyPair2.signingKey.compressedPublicKey
+
+    console.log(spendingPublicKey);
+    console.log(viewingPublicKey);
+  };
 
   const signMessageBlock = () => {
     return (
       <SignContent>
         <SignTitle>Generate and Publish Stealth Keys</SignTitle>
-        <InfoBlockText>Use the button below to complete the setup process</InfoBlockText>
+        <InfoBlockText>
+          Use the button below to complete the setup process
+        </InfoBlockText>
         <DFlex>
           <MAuto>
             <SetupButton onClick={signMessage}>Setup</SetupButton>
@@ -142,6 +163,7 @@ const SetupPage = () => {
 
   return (
     <Container>
+      <Toaster position="top-right" reverseOrder={false} />
       <Heading>Setup</Heading>
       <InfoBlock>
         {!installedSnap && connectWalletBlock()}
