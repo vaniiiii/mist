05e895d7d62dc6d5e3306a62e9384c0f27f0f822
vaniiiii <ivandzambas@gmail.com>
2024-06-02 07:46:33 +0200

    Hardhat config update

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -9,7 +9,7 @@ const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.24",
     settings: {
-      evmVersion: "shanghai",
+      evmVersion: "paris",
       optimizer: {
         enabled: true,
         runs: 200,
@@ -17,11 +17,19 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    // npx hardhat verify --network networkName {contractAddress} [{constructor arguments}]
-    apiKey:
-      process.env.ETHERSCAN_API_KEY !== undefined
-        ? process.env.ETHERSCAN_API_KEY
-        : "",
+    apiKey: {
+      neon_devnet: "test",
+    },
+    customChains: [
+      {
+        network: "neon_devnet",
+        chainId: 245022926,
+        urls: {
+          apiURL: "https://devnet-api.neonscan.org/hardhat/verify",
+          browserURL: "https://devnet.neonscan.org",
+        },
+      },
+    ],
   },
   sourcify: {
     enabled: true,
@@ -34,11 +42,43 @@ const config: HardhatUserConfig = {
           process.env.BASE_URL || "https://developer-access-mainnet.base.org",
       },
     },
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "https://rpc.sepolia.dev",
+      chainId: 11155111,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     base_sepolia: {
       url: process.env.BASE_SEPOLIA_URL || "https://sepolia.base.org",
       chainId: 84532,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    optimism_sepolia: {
+      url: process.env.OPTIMISM_SEPOLIA_URL || "https://sepolia.optimism.io",
+      chainId: 11155420,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    bsc_testnet: {
+      url:
+        process.env.BSC_TESTNET_URL ||
+        "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    linea_sepolia: {
+      url: process.env.LINEA_SEPOLIA_URL || "https://rpc.sepolia.linea.build",
+      chainId: 59141,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    neon_devnet: {
+      url: "https://devnet.neonevm.org",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 245022926,
     },
     base: {
       chainId: 8453,
